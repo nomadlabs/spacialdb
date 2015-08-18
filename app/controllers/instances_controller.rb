aa621cc311964bbb2b0b6a2afd89bcf0c87b135d
Fraboeni <franziska.boenisch.fb@gmail.com>
2015-08-18 13:15:09 +0200

    add validation to instance.region

--- app/controllers/instances_controller.rb
+++ app/controllers/instances_controller.rb
@@ -1,6 +1,6 @@
 class InstancesController < ApplicationController
   before_filter :authenticate_user!
-
+  before_action :set_global, only: [:new, :create]
   before_action :set_instance, only: [:show, :edit, :update, :destroy]
   respond_to :html
 
@@ -11,31 +11,37 @@ class InstancesController < ApplicationController
 
   def show
   end
+
   #get the content of the form to make a new subscription
   def new
     @instance = Instance.new
-    @plans = Plan.all
-    @plan = Plan.find_by_id(params[:plan]) || @plans.first
-    @regions = Region.order(:slug)
     respond_with(@instance, @plans)
   end
 
   def create 
+
     #go on and do the subscription
     logger.info instance_params
     #too early: should better be after the checkout
-    params.permit!.merge(
-      stripe_token: instance_params[:stripeToken]
-    )
-    
+    # params.permit!.merge(
+    #   stripe_token: instance_params[:stripeToken]
+    # )
+    params.permit!    
+
     #instance should better be after the payment
     @instance = current_user.instances.new(instance_params)
     @instance.subscription = CreateSubscription.call(params)
-    @instance.region_id = get_region_id
+    @instance.region_id = get_region_id 
     @instance.save
-    respond_with(@instance)   
+    respond_with(@instance)
+
   end
 
+  #stub method- to be called from the create function
+  #should return error if the hostname already exists
+  def host_exists
+      true
+  end
 
   # checks that the user does not have 2 instances with the same name.
   def check_hostname
@@ -43,6 +49,15 @@ class InstancesController < ApplicationController
   end
 
   private
+
+    # Set global variables on every reload
+    def set_global
+      @plans = @plans || Plan.all
+      @plan = Plan.find_by_id(params[:plan]) || @plans.first
+      @regions = Region.order(:slug)
+      @region = @regions.first.slug
+    end
+
     def set_instance
       @instance = current_user.instances.find(params[:id])
     end
@@ -52,7 +67,7 @@ class InstancesController < ApplicationController
     end
 
     def get_region_id
-      theslug = params[:region]
-      Region.find_by(slug: theslug).id
+      leslug = params[:region]
+      Region.find_by(slug: leslug).id
     end
 end
--- app/models/instance.rb
+++ app/models/instance.rb
@@ -2,4 +2,5 @@ class Instance < ActiveRecord::Base
   has_one :subscription
   belongs_to :region
   validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
+  validates :region, presence: true
 end
