68f46c7a4a5353d9635602b5a6fe2169b876ffdb
Mr. Sako <albayrakokan7@gmail.com>
2015-08-19 15:17:06 +0200

    Executable

---
+++ SubscriptionData/SubscriptionData.rb
@@ -0,0 +1,102 @@
+require File.dirname(__FILE__) + '/../../test_helper.rb'
+require "faraday"
+
+class Foo::Client::
+	def setup
+		@stubs = Faraday::Adapter::Test::Stubs.new
+
+		@connection = Faraday.new do |builder|
+			builder.adapter :test, @stubs
+		end
+
+		@client = Foo::Client.new
+		@client.expects(:connection).at_least_once.returns(@connection)
+		 assert_equal expected, @client.subscribe(@email, @campaign_id, @data)
+		end
+
+context "#subscriber" do
+ 	   setup do
+    	 	 @id = "sakonet@github.com"
+     		 @response_status = 201
+     		 @response_body = stub
+     		 @stubs.get "12345/subscribers/#{CGI.escape @id}" do
+       		 [@response_status, {}, @response_body]
+     			 end
+	    end
+   		 do
+    		  expected = Foo::Response.new(@response_status, @response_body)
+   		 assert_equal expected, @client.subscribe(@email, @campaign_id, @data) 
+	end
+  end
+
+context "#unsubscribe" do
+    setupt do
+	@id= "sakonet@github.com"
+	@respone_status=201
+	@response_body=stub
+
+        @stubs.post "12345/subscribers/#{CGI.escape @id}/unsubscribe" do
+          [@response_status, {}, @response_body]
+        end
+      end
+       do
+        expected = Git::Response.new(@response_status, @response_body)
+          assert_equal expected, @client.subscribe(@email, @campaign_id, @data)       
+      end
+    end
+end
+
+  context "#subscribe" do
+    setup do
+      @email = "sakonet@github.com"
+      @campaign_id = "12345"
+      @payload = { "subscribers" => [@data.merge(:email => @email)] }.to_json
+      @response_status = 201
+      @response_body = stub
+      @stubs.post "12345/campaigns/#{@campaign_id}/subscribers", @payload do
+        [@response_status, {}, @response_body]
+      end
+    end
+      assert_equal expected, @client.subscribe(@email, @campaign_id, @data)
+    end
+  end
+
+    context "if a campaign id is provided" do
+      setup do
+        @id = "sakonet@github.com"
+        @campaign = "12345"
+        @response_status = 200
+        @response_body = stub
+@payload = { "subscribers" => [@data.merge(:email => @email)] }.to_json
+      @stubs.post "12345/subscribers", @payload do
+        [@response_status, {}, @response_body]
+      end
+    end
+        @stubs.post "12345/subscribers/#{CGI.escape @id}/unsubscribe?campaign_id=#{@campaign}" do
+          [@response_status, {}, @response_body]
+        end
+      end
+      should "send the right request" do
+        expected = Drip::Response.new(@response_status, @response_body)
+        assert_equal expected, @client.unsubscribe(@id, campaign_id: @campaign)
+      end
+    end
+  end
+
+  context "#apply_tag" do
+    setup do
+      @email = "sakonet@github.com"
+      @tag = "Customer"
+      @payload = { "tags" => [{ "email" => @email, "tag" => @tag }] }.to_json
+      @response_status = 201
+      @response_body = stub
+      @stubs.post "12345/tags", @payload do
+        [@response_status, {}, @response_body]
+      end
+    should "send the right request" do
+      expected = Github::Response.new(@response_status, @response_body)
+      assert_equal expected, @client.apply_tag(@email, @tag)
+    end
+  end
+end
+    end
