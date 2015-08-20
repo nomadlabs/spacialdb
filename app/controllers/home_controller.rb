4ad6e40e95af2cd780c44e67f98f51262c1c9905
Fraboeni <franziska.boenisch.fb@gmail.com>
2015-08-20 13:57:34 +0200

    change button name if user logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,16 @@ class HomeController < ApplicationController
   respond_to :html
 
   def index
+      @button_name = button_name
     @plans = Plan.all.order(:id)
     respond_with(@plans)
+  end
+
+  def button_name
+    if user_signed_in?
+        "Get Plan"
+    else
+        "Sign up"
+    end
   end
 end
