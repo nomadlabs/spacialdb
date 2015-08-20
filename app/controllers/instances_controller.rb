072f4b4b1b388009955b87ca3c6c6a8ac3947128
Fraboeni <franziska.boenisch.fb@gmail.com>
2015-08-20 11:11:09 +0200

    add error messages on edit

--- app/controllers/instances_controller.rb
+++ app/controllers/instances_controller.rb
@@ -30,9 +30,13 @@ class InstancesController < ApplicationController
 
   def update
     params.permit(:instance, :region)
-    @instance.update_attributes(name: params[:instance][:name], region_id: get_region_id )
-    flash[:notice] = 'Instance was successfully updated.'
-    redirect_to @instance
+    if @instance.update_attributes(name: params[:instance][:name], region_id: get_region_id )
+      flash[:notice] = 'Instance was successfully updated.'
+      redirect_to @instance
+    else 
+      flash[:error] =  "Hostname not valid."
+      redirect_to edit_instance_path(@instance)     
+    end 
   end
 
   def create 
