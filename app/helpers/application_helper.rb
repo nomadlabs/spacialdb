d4bdc391f1885ad1bf62f9656e12f5084034420b
Jenny Gebske <jennifer@gebske.de>
2015-08-31 13:51:13 +0200

    remove empty class (take account of a Stackoverflow comment)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
   end
 
   def nav_link_to(link_text, link_path)
-    class_name = current_page?(link_path) ? 'active' : ''
+    class_name = current_page?(link_path) ? 'active' : nil
     content_tag(:li, :class => class_name) do
       link_to link_text, link_path
     end
