d1a669c7851623bcdffc3029f7a22c0f60cee09b
Jenny Gebske <jennifer@gebske.de>
2015-08-31 13:13:36 +0200

    add nav_link_to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
       :notice => 'alert-info'
     }[flash_type.to_sym] || flash_type.to_s
   end
+
+  def nav_link_to(link_text, link_path)
+    class_name = current_page?(link_path) ? 'active' : ''
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
+  end
+
 end
